7492368fb0631455d0641a1744925a2a9ae8a947
EnesBicici <148039789+EnesBicici@users.noreply.github.com>
2024-05-13 10:15:33 +0300

    Add files via upload

--- tarifekle.py
+++ tarifekle.py
@@ -31,17 +31,26 @@ class TarifEkleSayfa(QWidget):
         pisirme = self.form.pisirmeBox.value()
         kisi = self.form.kisiBox.value()
         tarifisim = self.form.tarifAdLine.text()
+        if not tarifisim:
+            QMessageBox.warning(self, "tarif","Tarif ismi boş geçilemez.", QMessageBox.Ok)
+            return
 
         malzemeler = []
         for i in range(1,self.malzemesayisi+1):
             line = getattr(self.form, f'malzeme{i}Line')
             malzeme = line.text()
+            if not malzeme:
+                QMessageBox.warning(self, "tarif","Malzeme boş geçilemez.", QMessageBox.Ok)
+                return
             malzemeler.append(malzeme)
         
         adimlar = []
         for i in range(1,self.adimsayisi+1):
             line = getattr(self.form, f'adim{i}Line')
             adim = line.text()
+            if not adim:
+                QMessageBox.warning(self, "tarif","Aşama boş geçilemez.", QMessageBox.Ok)
+                return
             adimlar.append(adim)
 
         Tarif.ekle(tarifisim, hazirlama, pisirme, kisi, malzemeler, adimlar)
@@ -51,28 +60,52 @@ class TarifEkleSayfa(QWidget):
         
     def malzemelineguncelle(self):
         yenimalzemesayisi = self.form.malzemeBox.value()
-        if (yenimalzemesayisi < self.malzemesayisi):
-            label = getattr(self.form, f'malzeme{self.malzemesayisi}label')
-            line = getattr(self.form, f'malzeme{self.malzemesayisi}Line')
-            line.setVisible(False)
-            label.setVisible(False)
+        if self.malzemesayisi == yenimalzemesayisi:
+            return
+    
+        if yenimalzemesayisi == 8:
+            for i in range(1,9):
+                label = getattr(self.form, f'malzeme{i}label')
+                line = getattr(self.form, f'malzeme{i}Line')
+                line.setVisible(True)
+                label.setVisible(True)
         else:
-            label = getattr(self.form, f'malzeme{yenimalzemesayisi}label')
-            line = getattr(self.form, f'malzeme{yenimalzemesayisi}Line')
-            line.setVisible(True)
-            label.setVisible(True)
+            for i in range(1,yenimalzemesayisi+1):
+                label = getattr(self.form, f'malzeme{i}label')
+                line = getattr(self.form, f'malzeme{i}Line')
+                line.setVisible(True)
+                label.setVisible(True)
+
+            for i in range(yenimalzemesayisi+1,9):
+                label = getattr(self.form, f'malzeme{i}label')
+                line = getattr(self.form, f'malzeme{i}Line')
+                line.setVisible(False)
+                label.setVisible(False)
         self.malzemesayisi = yenimalzemesayisi
 
     def adimlineguncelle(self):
         yeniadimsayisi = self.form.adimBox.value()
-        if (yeniadimsayisi < self.malzemesayisi):
-            label = getattr(self.form, f'adim{self.adimsayisi}label')
-            line = getattr(self.form, f'adim{self.adimsayisi}Line')
-            line.setVisible(False)
-            label.setVisible(False)
+
+        if self.adimsayisi == yeniadimsayisi:
+            return
+    
+        if yeniadimsayisi == 9:
+            for i in range(1,10):
+                label = getattr(self.form, f'adim{i}label')
+                line = getattr(self.form, f'adim{i}Line')
+                line.setVisible(True)
+                label.setVisible(True)
         else:
-            label = getattr(self.form, f'adim{yeniadimsayisi}label')
-            line = getattr(self.form, f'adim{yeniadimsayisi}Line')
-            line.setVisible(True)
-            label.setVisible(True)
+            for i in range(1,yeniadimsayisi+1):
+                label = getattr(self.form, f'adim{i}label')
+                line = getattr(self.form, f'adim{i}Line')
+                line.setVisible(True)
+                label.setVisible(True)
+
+            for i in range(yeniadimsayisi+1,10):
+                label = getattr(self.form, f'adim{i}label')
+                line = getattr(self.form, f'adim{i}Line')
+                line.setVisible(False)
+                label.setVisible(False)
+
         self.adimsayisi = yeniadimsayisi
